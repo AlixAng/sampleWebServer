d1c9ac4f49d139a9399a1a8668dd73fd3c4890d4
AlixA <Alix.Angoran@ge.com>
2018-09-07 14:42:43 -0700

    remove cross origin

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const config = require('./config/config.js');
 const app = express();
 require('./routes/route')(app);
 
-
-// middleware to allow cross origin
-app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Header, Origin, X-request-With, Content-Type, Accept');
-  next();
-});
-
 var server = app.listen(config.server.port, ()=>{
   console.log('we are live at:', config.server.host, server.address().port);
 });
