575e5c466cb8b7fa86df8f4711bb4965c58872db
AlixA <Alix.Angoran@ge.com>
2018-09-07 14:16:01 -0700

    add middleware for  cross origin

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const config = require('./config/config.js');
 const app = express();
 require('./routes/route')(app);
 
+//middleware to use static file
+app.use(express.static(path.join(__dirname+'public')));
+
+// middleware to allow cross origin
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Header, Origin, X-request-With, Content-Type, Accept');
+  next();
+});
+
 var server = app.listen(config.server.port, ()=>{
   console.log('we are live at:', config.server.host, server.address().port);
 });
