6817fbf70e45cc53da6c9b1d6c0feb8c684d8466
AlixA <Alix.Angoran@ge.com>
2018-09-07 14:52:34 -0700

    add 404 route

--- routes/route.js
+++ routes/route.js
@@ -6,7 +6,10 @@ module.exports = function(app){
   });
 
   app.get('/image',function(req,res) {
-    // res.send('Queen Nzinga !');
     res.sendFile(path.join(__dirname, '../public/Nzinga.jpg'));
   });
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/404.jpg'));
+  })
 }
